c56a63a69dde9455ca89e0d5be3bf99942c92001
Rayon Hunte <rayon.hunte@gmail.com>
2017-11-02 12:05:50 -0400

    end of arrow

--- arrow.js
+++ arrow.js
@@ -12,3 +12,31 @@ loopOver10(function(number){
 
 loopOver10(number => console.log(number));
 
+loopOver10(number => {
+  console.log(number);
+  console.log(number * 10);
+})
+
+var noParamFunc = () => console.log("HI");
+
+noParamFunc();
+
+// included return function
+var multiParmFunc = (left, right) => left + right;
+// if curly must add return
+
+console.log(multiParmFunc(10,20));
+
+
+var obj = {
+  name: "Nelson",
+
+  showName(){
+    console.log("My name is " + this.name);
+    setTimeout(()=>{
+      console.log("My name is now ", this.name);
+    },1000);
+  }
+}
+
+obj.showName();
